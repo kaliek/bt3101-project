a2719cf8e759fbf249acb0735ec41c77605ddb2a
Shadowsong27 <syk950527@gmail.com>
2017-10-04 18:14:45 +0800

    added name dictionary for name parsing

--- faculty_list_crawler/main.py
+++ faculty_list_crawler/main.py
@@ -20,10 +20,31 @@ from bs4 import BeautifulSoup
 from bs4.element import Tag
 from pymongo import MongoClient
 from common import UnknownStatusException
-
+from nameparser import HumanName
 import logging
 import os
+from html.parser import HTMLParser
+import string
 
+
+class MyHTMLParser(HTMLParser):
+
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.data_list = []
+
+    def handle_starttag(self, tag, attrs):
+        pass
+
+    def handle_endtag(self, tag):
+        pass
+
+    def handle_data(self, data):
+        if data != " ":
+            self.data_list.append(data)
+
+    def get_list(self):
+        return self.data_list
 
 
 class Downloader:
@@ -31,8 +52,8 @@ class Downloader:
     """
         handles the downloading of faculty page
     """
-    PATH_TO_TEMP_WEB = r'../temp'
-    FORMAT_TEMP_WEB = "../temp/{}-{}.html"
+    PATH_TO_TEMP_WEB = r'temp'
+    FORMAT_TEMP_WEB = "temp/{}-{}.html"
 
     def __init__(self):
         self.db_handler = DatabaseHandler()
@@ -54,38 +75,104 @@ class Downloader:
 
 class Analyser:
 
+
     """
         given the url of the faculty list
         output the data in structured format
     """
+
+    PATH_TO_TEMP_WEB = r'temp'
+
     def __init__(self):
-        self.soup = None
         self.soup_memory = []
+        self.translator = str.maketrans('', '', string.punctuation)
+        self.name_dictionary = self.load_name_dictionary()
 
-    def _build_soup(self):
-        self.soup = BeautifulSoup(open('../temp/University of Colorado – Boulder.html', 'r', encoding='utf-8').read(), 'lxml')
-        # self.soup = BeautifulSoup(open('../temp/University College London.html', 'r', encoding='utf-8').read(), 'lxml')
+    def load_name_dictionary(self):
+        name_list = open("name_dictionary.txt").read().split("\n")
+        name_list = list(map(lambda x: x.split(',')[0], name_list))
+        return name_list
 
-    def get_body(self):
-        return self.soup.body
+    def _build_soup(self, file_name):
+        try:
+            file = open(os.path.join(self.PATH_TO_TEMP_WEB, file_name), encoding='utf-8').read()
+        except UnicodeDecodeError:
+            file = open(os.path.join(self.PATH_TO_TEMP_WEB, file_name), encoding='latin-1').read()
+        return BeautifulSoup(file, 'lxml')
 
     def find_most_children(self, element):
         for child in element.contents:
             if isinstance(child, Tag):
-                count = self.find_number_of_direct_tags(child)
+                count = self._find_number_of_direct_tags(child)
                 self.soup_memory.append((count, child))
                 self.find_most_children(child)
 
-    def find_number_of_direct_tags(self, element):
+    @staticmethod
+    def _find_number_of_direct_tags(element):
         counter = 0
         for child in element.contents:
             if isinstance(child, Tag):
                 counter += 1
         return counter
 
-    def get_highest_elemment(self):
+    def get_highest_element(self):
         self.soup_memory.sort(key=lambda x:x[0], reverse=True)
-        print(self.soup_memory[0])
+        return self.soup_memory[0][1]
+
+    # TODO: the bulk of the project: how to parse??????
+    def _parse_element(self, element):
+        for item in element.contents:
+            if isinstance(item, Tag):
+                parser = MyHTMLParser()
+                feed_content = str(item).replace("\n", "").strip().replace(u'\xa0', u' ')
+                parser.feed(feed_content)
+                data_list = parser.get_list()
+
+                if not data_list:
+                    continue
+
+                token_list = parser.get_list()
+
+                if self.is_valid_data_row(token_list):
+                    logging.info(token_list)
+                    logging.info(len(token_list))
+
+            break
+
+    def is_valid_data_row(self, token_list):
+        # test first few token are names? if not not relevant, allow friendly kills
+        first_token = " ".join(token_list[:2]).split(" ")
+        for item in first_token:
+            if item in self.name_dictionary:
+                return True
+        return False
+
+    def is_punctuation_token(self, token):
+        if len(token) <= 2:
+            return not token.isalpha()
+        return False
+
+
+
+                # # remove extra space, newlines
+                # text = item.text.replace("\n", "^").strip()
+                # # check header irrelevant text
+                #
+                # # potential separator: '•', ' - '
+
+    def clear_memory(self):
+        self.soup_memory = []
+
+    def analyse_web(self):
+        files = os.listdir(self.PATH_TO_TEMP_WEB)
+        for temp_file in files:
+            soup = self._build_soup(temp_file)
+            body_tag = soup.body
+            self.find_most_children(body_tag)
+            element = self.get_highest_element()
+            self.clear_memory()
+            self._parse_element(element)
+            break
 
 
 class DatabaseHandler:
@@ -131,4 +218,5 @@ class DatabaseHandler:
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    Downloader().process_requests()
+    # Downloader().process_requests()
+    Analyser().analyse_web()
